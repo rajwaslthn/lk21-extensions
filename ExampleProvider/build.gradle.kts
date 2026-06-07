35ee50d365d6576f920eb40e02ef462289a14749
rajwaslthn <rajwasulthanyogi@gmail.com>
2026-06-07 14:45:40 +0700

    Complete LK21 provider1

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -7,27 +7,13 @@ dependencies {
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
+    description = "Nonton film Indonesia di LK21"
+    authors = listOf("rajwaslthn")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
     requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    language = "id"
+    iconUrl = "https://www.google.com/s2/favicons?domain=lk21online.mom&sz=256"
 }
 
 android {
